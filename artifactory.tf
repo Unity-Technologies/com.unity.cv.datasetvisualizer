10d6cb374c632784b0d3c58b2cae64ef343e95e1
Ruiyu Zhnag <etain42@gmail.com>
2022-01-07 15:23:34 -0800

    Add the terraform resources

---
+++ artifactory.tf
@@ -0,0 +1,21 @@
+﻿resource "artifactory_group" "ai-cv-tools-local" {
+  name          = "ai-cv-tools-local"
+  description  = "AI Computer Vision Tools team read/write group"
+}
+
+resource "artifactory_permission_target" "ads-creative-tools-rw" {
+  name = "ads-creative-tools-rw"
+
+  repo {
+    repositories = [
+      "ads-npm-prod-local",
+    ]
+
+    actions {
+      groups {
+        name        = artifactory_group.ai-cv-tools-local.name
+        permissions = ["read", "write", "delete", "annotate"]
+      }
+    }
+  }
+}
